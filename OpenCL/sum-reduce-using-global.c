6638c17db48c122f6a46f4a82cfac4e437471ade
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2023-06-28 14:43:11 +0300

    OpenCL simple exmaples
    - compute sum by reduction using only global id

---
+++ OpenCL/sum-reduce-using-global.c
@@ -0,0 +1,31 @@
+#define SCRATCH_SIZE 1024
+
+__kernel void sum_by_reduction(__global float* in, __global float* out, uint32_t len) {
+
+    __local float scratch[SCRATCH_SIZE];
+
+    size_t global_idx = get_global_id(0);
+
+    if (global_idx >= len) return;
+
+    // Perform a local operation on the input data
+    scratch[global_idx] = in[global_idx];
+
+    // Synchronize threads within the work-group to ensure all local data is available
+    barrier(CLK_GLOBAL_MEM_FENCE);
+
+    // Perform a reduction operation on the local data
+    for (size_t stride = get_global_size(0) / 2; stride > 0; stride /= 2) {
+        if (global_idx < stride) {
+            scratch[global_idx] += scratch[global_idx + stride];
+        }
+
+        // Synchronize threads within the work-group to ensure all partial sums are computed
+        barrier(CLK_GLOBAL_MEM_FENCE);
+    }
+
+    // Write the result of the reduction to the output buffer
+    if (global_idx == 0) {
+        out[global_idx] = scratch[0];
+    }
+}
