9a65d1449d16596a3c6b6ebb94be14ffd4906574
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2024-08-27 17:30:27 +0300

    simple refactor

--- C/leetcode&interviews/sockets/server_socket.c
+++ C/leetcode&interviews/sockets/server_socket.c
@@ -6,38 +6,32 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <assert.h>
 #define PORT 8080
+
 int main(int argc, char const* argv[])
 {
-    int server_fd, new_socket;
-    ssize_t valread;
+
+    int valread;
     struct sockaddr_in address;
-    int opt = 1;
+
     socklen_t addrlen = sizeof(address);
     char buffer[1024] = { 0 };
     char* hello = "Hello from server";
 
     // Creating socket file descriptor
+    int server_fd = -1;
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET,
-                   SO_REUSEADDR | SO_REUSEPORT, &opt,
-                   sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
     // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr*)&address,
-             sizeof(address))
-        < 0) {
+    if (bind(server_fd, (struct sockaddr*)&address,sizeof(address)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
@@ -45,15 +39,13 @@ int main(int argc, char const* argv[])
         perror("listen");
         exit(EXIT_FAILURE);
     }
-    if ((new_socket
-                 = accept(server_fd, (struct sockaddr*)&address,
-                          &addrlen))
-        < 0) {
+    int new_socket = -1;
+    if ((new_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen)) < 0) {
         perror("accept");
         exit(EXIT_FAILURE);
     }
-    valread = read(new_socket, buffer,
-                   1024 - 1); // subtract 1 for the null
+    valread = read(new_socket, buffer, 1024);
+    assert(valread > 0);
     // terminator at the end
     printf("%s\n", buffer);
     send(new_socket, hello, strlen(hello), 0);
@@ -65,3 +57,4 @@ int main(int argc, char const* argv[])
     close(server_fd);
     return 0;
 }
+
