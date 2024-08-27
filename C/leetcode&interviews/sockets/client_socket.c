05b4db62ec30031315521ae2dcf94941be40819b
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2024-08-27 17:31:07 +0300

    Update client_socket.c

--- C/leetcode&interviews/sockets/client_socket.c
+++ C/leetcode&interviews/sockets/client_socket.c
@@ -7,17 +7,17 @@
 #include <unistd.h>
 #define PORT 8080
 
-int main(int argc, char const* argv[])
-{
-    int status, valread, client_fd;
-    struct sockaddr_in serv_addr;
-    char* hello = "Hello from client";
-    char buffer[1024] = { 0 };
+int main(int argc, char const* argv[]) {
+
+
+
+    int client_fd = -1;
     if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\n Socket creation error \n");
         return -1;
     }
 
+    struct sockaddr_in serv_addr;
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
@@ -27,16 +27,20 @@ int main(int argc, char const* argv[])
         printf("\nInvalid address/ Address not supported \n");
         return -1;
     }
-
+    int status = -1;
     if ((status = connect(client_fd, (struct sockaddr*)&serv_addr,
-                           sizeof(serv_addr))) < 0) {
+                          sizeof(serv_addr))) < 0) {
         printf("\nConnection Failed \n");
         return -1;
     }
+
+    char* hello = "Hello from client";
     send(client_fd, hello, strlen(hello), 0);
     printf("Hello message sent\n");
-    valread = read(client_fd, buffer,
-                   1024 - 1); // subtract 1 for the null
+    int valread = -1;
+
+    char buffer[1024] = { 0 };
+    valread = read(client_fd, buffer, 1024);
     // terminator at the end
     printf("%s\n", buffer);
 
@@ -44,3 +48,4 @@ int main(int argc, char const* argv[])
     close(client_fd);
     return 0;
 }
+
