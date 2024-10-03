ac1a76320fbcfb61cb22289c1341548320873f9f
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2024-10-03 21:31:54 +0300

    Update ch2b.py

--- DataEngineering/introduction/ch2b.py
+++ DataEngineering/introduction/ch2b.py
@@ -137,3 +137,29 @@ film_df = film_df.assign(
     rental_rate_dollar=rental_rate_expanded[0],
     rental_rate_cents=rental_rate_expanded[1],
 )
+
+#another part
+
+# Use groupBy and mean to aggregate the column
+ratings_per_film_df = rating_df.groupBy('film_id').mean('rating')
+
+# Join the tables using the film_id column
+film_df_with_ratings = film_df.join(
+    ratings_per_film_df,
+    film_df.film_id==ratings_per_film_df.film_id
+)
+
+# Show the 5 first results
+print(film_df_with_ratings.show(5))
+'''
++-------+----------------+--------------------+------------+-----------+--------------------+---------------+-----------+------+----------------+------+--------------------+--------------------+--------------------+-------+------------------+
+|film_id|           title|         description|release_year|language_id|original_language_id|rental_duration|rental_rate|length|replacement_cost|rating|         last_update|    special_features|            fulltext|film_id|       avg(rating)|
++-------+----------------+--------------------+------------+-----------+--------------------+---------------+-----------+------+----------------+------+--------------------+--------------------+--------------------+-------+------------------+
+|      1|ACADEMY DINOSAUR|A Epic Drama of a...|        2006|          1|                null|              6|       0.99|    86|           20.99|    PG|2017-09-10 17:46:...|[Deleted Scenes, ...|'academi':1 'batt...|      1| 4.646341463414634|
+|      2|  ACE GOLDFINGER|A Astounding Epis...|        2006|          1|                null|              3|       4.99|    48|           12.99|     G|2017-09-10 17:46:...|[Trailers, Delete...|'ace':1 'administ...|      2|4.7555555555555555|
+|      3|ADAPTATION HOLES|A Astounding Refl...|        2006|          1|                null|              7|       2.99|    50|           18.99| NC-17|2017-09-10 17:46:...|[Trailers, Delete...|'adapt':1 'astoun...|      3| 4.209677419354839|
+|      4|AFFAIR PREJUDICE|A Fanciful Docume...|        2006|          1|                null|              5|       2.99|   117|           26.99|     G|2017-09-10 17:46:...|[Commentaries, Be...|'affair':1 'chase...|      4|               4.2|
+|      5|     AFRICAN EGG|A Fast-Paced Docu...|        2006|          1|                null|              6|       2.99|   130|           22.99|     G|2017-09-10 17:46:...|    [Deleted Scenes]|'african':1 'chef...|      5|  4.63448275862069|
++-------+----------------+--------------------+------------+-----------+--------------------+---------------+-----------+------+----------------+------+--------------------+--------------------+--------------------+-------+------------------+
+only showing top 5 rows
+'''
