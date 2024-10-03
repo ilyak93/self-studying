38464e2307618f0539d2331429ac8c9fd0896ea7
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2024-10-03 20:39:17 +0300

    Create ch1a.py

---
+++ DataEngineering/introduction/ch1a.py
@@ -0,0 +1,19 @@
+# Import required libraries
+import pandas as pd
+from sqlalchemy import create_engine
+
+# Assume db_engine is already created
+# If not, you would create it like this:
+# db_engine = create_engine('your_database_connection_string_here')
+
+# Execute SQL query and load results into a DataFrame
+data = pd.read_sql("""
+SELECT first_name, last_name FROM "Customer"
+ORDER BY last_name, first_name
+""", db_engine)
+
+# Show the first 3 rows of the DataFrame
+print(data.head(3))
+
+# Show the info of the DataFrame
+print(data.info())
