b46bd5c4b4c86b563526d61fcfdc611f19097cf2
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2024-10-04 16:27:38 +0300

    Update ch4.py

--- DataEngineering/introduction/ch4.py
+++ DataEngineering/introduction/ch4.py
@@ -62,3 +62,31 @@ print(avg_rating_data)
 
 [99 rows x 2 columns]
 '''
+
+#part3
+course_data = extract_course_data(db_engines)
+
+# Print out the number of missing values per column
+print(course_data.isnull().sum())
+
+# The transformation should fill in the missing values
+def transform_fill_programming_language(course_data):
+    imputed = course_data.fillna({"programming_language": "R"})
+    return imputed
+
+transformed = transform_fill_programming_language(course_data)
+
+# Print out the number of missing values per column of transformed
+print(transformed.isnull().sum())
+'''
+course_id               0
+title                   0
+description             0
+programming_language    3
+dtype: int64
+course_id               0
+title                   0
+description             0
+programming_language    0
+dtype: int64
+'''
